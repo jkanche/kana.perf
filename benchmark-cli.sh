d05bf8b4a276d0e61d27d879abf81dae92659910
jkanche <jayaram.kancherla@gmail.com>
2022-01-30 21:53:24 -0800

    fix: a few more things and rerun

--- benchmark-cli.sh
+++ benchmark-cli.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+rm -rf ./cli-results
+
 cd scran-cli
 
 run_dataset () {
     echo "dataset : $1"
-    path="./data/$1"
+    path="../data/$1"
     echo "path: $path"
-    mkdir -p $1
+    mkdir -p "../cli-results/$1"
+    
     for i in {1..3}; do
         echo "run $i" 
-        /usr/bin/time -v ./build/scran -t 8 --skip-output $path/matrix.mtx.gz $path/features.tsv.gz > "./cli-results/$1/$i.log" 2>&1 
+        /usr/bin/time -v ./build/scran -t 8 --skip-output $path/matrix.mtx.gz $path/features.tsv.gz > "../cli-results/$1/$i.log" 2>&1 
     done
 }
-
-mkdir -p ./cli-results
 
 run_dataset "zeisel-brain"
 run_dataset "zilionis-lung"
